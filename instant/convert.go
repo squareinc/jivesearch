bce27d131f80046284d76bda9c6eb60949b2dbe7
Brent Adamson <6355304+brentadamson@users.noreply.github.com>
2018-03-09 21:52:34 -0700

    more conversion triggers

--- instant/convert.go
+++ instant/convert.go
@@ -48,16 +48,39 @@ func (c *Convert) setRegex() answerer {
 		"convert", "converter",
 	}
 
-	t := strings.Join(triggers, "|")
-	c.regex = append(c.regex, regexp.MustCompile(fmt.Sprintf(`^(?P<trigger>%s)$`, t)))
-	//c.regex = append(c.regex, regexp.MustCompile(fmt.Sprintf(`^(?P<trigger>%s) .*$`, t))) // not implemented yet
-	//c.regex = append(c.regex, regexp.MustCompile(fmt.Sprintf(`^.* (?P<trigger>%s)$`, t))) // not implemented yet
+	c.regex = append(c.regex, regexp.MustCompile(fmt.Sprintf(`^(?P<trigger>%s)$`, strings.Join(triggers, "|"))))
+
+	dataStorage := []string{
+		"bit", "byte",
+		"kilobit", "kibibit", "kilobyte", "kibibyte",
+		"megabit", "mebibit", "megabyte", "mebibyte",
+		"gigabit", "gibibit", "gigabyte", "gibibyte",
+		"terabit", "tebibit", "terabyte", "tebibyte",
+		"petabit", "pebibit", "petabyte", "pebibyte",
+		"kb", "kbit", "kibit", "kib",
+		"mb", "mbit", "mibit", "mib",
+		"gb", "gbit", "gibit", "gib",
+		"tb", "tbit", "tibit", "tib",
+		"pb", "pbit", "pibit", "pib",
+	}
+
+	for i, d := range dataStorage {
+		dataStorage[i] = d + "[s]?"
+	}
+
+	ds := strings.Join(dataStorage, "|")
+	t := fmt.Sprintf("[0-9 ]*?%v to[0-9 ]*?%v", ds, ds)
+
+	c.regex = append(c.regex, regexp.MustCompile(fmt.Sprintf(`^(?P<trigger>%s)(?P<remainder>.*)$`, t))) // "convert 50mb to kb"
+	c.regex = append(c.regex, regexp.MustCompile(fmt.Sprintf(`^(?P<remainder>.*)(?P<trigger>%s)$`, t)))
 
 	return c
 }
 
 func (c *Convert) solve() answerer {
 	// The caller is expected to provide the solution when triggered, preferably in JavaScript
+	// TODO: pass the remainder to our html template so that "50gb to mb" prefills the form with "50", "gb", and "mb"
+
 	return c
 }
 
@@ -81,6 +104,33 @@ func (c *Convert) tests() []test {
 	tests := []test{
 		{
 			query:    "convert",
+			expected: []Data{d},
+		},
+		{
+			query:    "convert 1mb to pbs",
+			expected: []Data{d},
+		},
+		/*
+			not passing for some reason...
+			{
+				query:    "50gb to mb converter",
+				expected: []Data{d},
+			},
+		*/
+		{
+			query:    "gb to mb",
+			expected: []Data{d},
+		},
+		{
+			query:    "petabytes to megabit",
+			expected: []Data{d},
+		},
+		{
+			query:    "50 gb to 100pb",
+			expected: []Data{d},
+		},
+		{
+			query:    "50gbs to mbs",
 			expected: []Data{d},
 		},
 	}
