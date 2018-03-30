712dd171e5888359138f02c8e52abe8e712c7a5b
Brent Adamson <6355304+brentadamson@users.noreply.github.com>
2018-03-30 15:05:10 -0600

    disable intraday charts

--- frontend/filters.go
+++ frontend/filters.go
@@ -224,7 +224,7 @@ func instantFormatter(sol instant.Data, r language.Region) string {
 		quote += `<div id="stock_chart" class="pure-u-1"></div>`
 		quote += `<div class="pure-u-1">
 			<div id="time_period_buttons" class="pure-button-group" role="group" aria-label="time select" style="margin-left:47px;">
-				<button id="day" class="pure-button">Day</button>&nbsp;&nbsp;
+				<button id="day" class="pure-button" disabled>Day</button>&nbsp;&nbsp;
 				<button id="week" class="pure-button">Week</button>&nbsp;&nbsp;
 				<button id="month" class="pure-button">Month</button>&nbsp;&nbsp;
 				<button id="ytd" class="pure-button">YTD</button>&nbsp;&nbsp;
