e1471d40b47fbfb660e5b6aab570e6708d6dea0f
Brent Adamson <6355304+brentadamson@users.noreply.github.com>
2018-07-29 10:45:10 -0600

    more fx triggers

--- instant/fx.go
+++ instant/fx.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -49,13 +50,25 @@ func (f *FX) setType() Answerer {
 }
 
 func (f *FX) setRegex() Answerer {
+	f.regex = append(f.regex, regexp.MustCompile(`^convert (?P<notional>\d+) (?P<from>.*) to (?P<to>.*)$`))
 	f.regex = append(f.regex, regexp.MustCompile(`^convert (?P<from>.*) to (?P<to>.*)$`))
+	f.regex = append(f.regex, regexp.MustCompile(`^(?P<notional>\d+) (?P<from>.*) to (?P<to>.*)$`))
+	f.regex = append(f.regex, regexp.MustCompile(`^(?P<from>.*) to (?P<to>.*)$`))
 	return f
 }
 
 func (f *FX) solve(r *http.Request) Answerer {
 	resp := &FXResponse{
 		Notional: 1,
+	}
+
+	if n, ok := f.remainderM["notional"]; ok {
+		nn, err := strconv.ParseFloat(n, 64)
+		if err != nil {
+			f.Err = err
+			return f
+		}
+		resp.Notional = nn
 	}
 
 	// make sure valid currencies were passed in
@@ -141,6 +154,47 @@ func (f *FX) tests() []test {
 						Notional: 1,
 						From:     fx.JPY,
 						To:       fx.USD,
+					},
+					Cache: true,
+				},
+			},
+		},
+		{
+			query: "125 EUR to JPY",
+			expected: []Data{
+				{
+					Type:      typ,
+					Triggered: true,
+					Solution: &FXResponse{
+						Response: &fx.Response{
+							Rates: []*fx.Rate{
+								{
+									Base:     fx.USD,
+									Currency: fx.BGN,
+									Rate:     .5944,
+								},
+								{
+									Base:     fx.USD,
+									Currency: fx.EUR,
+									Rate:     1.1625,
+								},
+								{
+									Base:     fx.USD,
+									Currency: fx.JPY,
+									Rate:     0.009,
+								},
+								{
+									Base:     fx.USD,
+									Currency: fx.USD,
+									Rate:     1.0,
+								},
+							},
+							DateTime: time.Date(2018, 07, 27, 0, 0, 0, 0, time.UTC),
+							Provider: fx.ECBProvider,
+						},
+						Notional: 125,
+						From:     fx.EUR,
+						To:       fx.JPY,
 					},
 					Cache: true,
 				},
