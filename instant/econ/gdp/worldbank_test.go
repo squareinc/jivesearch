ee837c783326f882a961debf93be41a9df5181e0
Brent Adamson <6355304+brentadamson@users.noreply.github.com>
2018-08-23 14:46:34 -0600

    fix tests

--- instant/econ/gdp/worldbank_test.go
+++ instant/econ/gdp/worldbank_test.go
@@ -1,7 +1,15 @@
 package gdp
 
-// can't seem to figure out the xml mocking...keep getting "no responder found"
-/*
+import (
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/jivesearch/jivesearch/instant/econ"
+)
+
 func TestFetch(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -15,14 +23,14 @@ func TestFetch(t *testing.T) {
 	for _, tt := range []struct {
 		name string
 		args
-		u    map[string]string
+		u    string
+		resp string
 		want *Response
 	}{
 		{
 			name: "basic",
-			u: map[string]string{
-				`http://api.worldbank.org/v2/countries/it/indicators/NY.GDP.MKTP.CD`: `<?xml version="1.0" encoding="utf-8"?><wb:data page="1" pages="2" per_page="50" total="58" lastupdated="2018-07-25" xmlns:wb="http://www.worldbank.org"><wb:data><wb:indicator id="NY.GDP.MKTP.CD">GDP (current US$)</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2017</wb:date><wb:value>1934797937411.33</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data><wb:data><wb:indicator id="NY.GDP.MKTP.CD">GDP (current US$)</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2016</wb:date><wb:value>1859383610248.72</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data></wb:data>`,
-			},
+			u:    `http://api.worldbank.org/v2/countries/IT/indicators/NY.GDP.MKTP.CD`,
+			resp: `<?xml version="1.0" encoding="utf-8"?><wb:data page="1" pages="2" per_page="50" total="58" lastupdated="2018-07-25" xmlns:wb="http://www.worldbank.org"><wb:data><wb:indicator id="NY.GDP.MKTP.CD">GDP (current US$)</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2017</wb:date><wb:value>1934797937411.33</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data><wb:data><wb:indicator id="NY.GDP.MKTP.CD">GDP (current US$)</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2016</wb:date><wb:value>1859383610248.72</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data></wb:data>`,
 			args: args{
 				country: "IT",
 				from:    time.Date(1930, 12, 31, 0, 0, 0, 0, time.UTC),
@@ -30,27 +38,16 @@ func TestFetch(t *testing.T) {
 			},
 			want: &Response{
 				History: []Instant{
-					{time.Date(2017, 12, 31, 0, 0, 0, 0, time.UTC), 18},
-					{time.Date(2003, 12, 31, 0, 0, 0, 0, time.UTC), 2},
+					{time.Date(2017, 12, 31, 0, 0, 0, 0, time.UTC), 1934797937411.33},
+					{time.Date(2016, 12, 31, 0, 0, 0, 0, time.UTC), 1859383610248.72},
 				},
-				Provider: TheWorldBankProvider,
+				Provider: econ.TheWorldBankProvider,
 			},
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
-			for k, v := range tt.u {
-				x := &WorldBankResponse{}
-				if err := xml.Unmarshal([]byte(v), x); err != nil {
-					t.Fatal(err)
-				}
-
-				responder, err := httpmock.NewXmlResponder(200, x)
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				httpmock.RegisterResponder("GET", k, responder) // no responder found????
-			}
+			responder := httpmock.NewStringResponder(200, tt.resp)
+			httpmock.RegisterResponder("GET", tt.u, responder) // no responder found????
 
 			w := &WorldBank{
 				HTTPClient: &http.Client{},
@@ -69,4 +66,3 @@ func TestFetch(t *testing.T) {
 
 	httpmock.Reset()
 }
-*/
--- instant/econ/population/worldbank_test.go
+++ instant/econ/population/worldbank_test.go
@@ -1,7 +1,15 @@
 package population
 
-// can't seem to figure out the xml mocking...keep getting "no responder found"
-/*
+import (
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/jivesearch/jivesearch/instant/econ"
+)
+
 func TestFetch(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
@@ -15,14 +23,14 @@ func TestFetch(t *testing.T) {
 	for _, tt := range []struct {
 		name string
 		args
-		u    map[string]string
+		u    string
+		resp string
 		want *Response
 	}{
 		{
 			name: "basic",
-			u: map[string]string{
-				`http://api.worldbank.org/v2/countries/it/indicators/SP.POP.TOTL?date=1930%3A2018`: `<?xml version="1.0" encoding="utf-8"?><wb:data page="1" pages="2" per_page="50" total="58" lastupdated="2018-07-25" xmlns:wb="http://www.worldbank.org"><wb:data><wb:indicator id="SP.POP.TOTL">Population, total</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2017</wb:date><wb:value>18</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data><wb:data><wb:indicator id="SP.POP.TOTL">Population, total</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2003</wb:date><wb:value>2</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data><wb:data><wb:indicator id="SP.POP.TOTL">Population, total</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>1994</wb:date><wb:value>4</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data></wb:data>`,
-			},
+			u:    `http://api.worldbank.org/v2/countries/IT/indicators/SP.POP.TOTL?date=1930%3A2018`,
+			resp: `<?xml version="1.0" encoding="utf-8"?><wb:data page="1" pages="2" per_page="50" total="58" lastupdated="2018-07-25" xmlns:wb="http://www.worldbank.org"><wb:data><wb:indicator id="SP.POP.TOTL">Population, total</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2017</wb:date><wb:value>18</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data><wb:data><wb:indicator id="SP.POP.TOTL">Population, total</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>2003</wb:date><wb:value>2</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data><wb:data><wb:indicator id="SP.POP.TOTL">Population, total</wb:indicator><wb:country id="IT">Italy</wb:country><wb:countryiso3code>ITA</wb:countryiso3code><wb:date>1994</wb:date><wb:value>4</wb:value> <wb:unit/> <wb:obs_status/> <wb:decimal>0</wb:decimal></wb:data></wb:data>`,
 			args: args{
 				country: "IT",
 				from:    time.Date(1930, 12, 31, 0, 0, 0, 0, time.UTC),
@@ -34,24 +42,13 @@ func TestFetch(t *testing.T) {
 					{time.Date(2003, 12, 31, 0, 0, 0, 0, time.UTC), 2},
 					{time.Date(1994, 12, 31, 0, 0, 0, 0, time.UTC), 4},
 				},
-				Provider: TheWorldBankProvider,
+				Provider: econ.TheWorldBankProvider,
 			},
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
-			for k, v := range tt.u {
-				x := &WorldBankResponse{}
-				if err := xml.Unmarshal([]byte(v), x); err != nil {
-					t.Fatal(err)
-				}
-
-				responder, err := httpmock.NewXmlResponder(200, x)
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				httpmock.RegisterResponder("GET", k, responder) // no responder found????
-			}
+			responder := httpmock.NewStringResponder(200, tt.resp)
+			httpmock.RegisterResponder("GET", tt.u, responder) // no responder found????
 
 			w := &WorldBank{
 				HTTPClient: &http.Client{},
@@ -70,4 +67,3 @@ func TestFetch(t *testing.T) {
 
 	httpmock.Reset()
 }
-*/
