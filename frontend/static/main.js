3f3abf79bc19c5b855f6bdc6870651c52588ce9a
Brent Adamson <6355304+brentadamson@users.noreply.github.com>
2018-09-04 18:55:34 -0600

    chrome instructions

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -167,5 +167,48 @@ $(document).ready(function() {
   $("#map, #maps").on("click", function(){
     changeParamAndRedirect("t", "maps");
   });
+
+  var b = browser();
+  $("#add_to_browser").html("Add "+brand+" to " + b);
+
+  $("#add_to_browser").on("click", function(){
+    $("#instructions").toggle();
+    if (b == "Chrome"){
+      $("#chrome_instructions").show();
+    }
+  });
 });
 
+
+
+// Browser detection for instructions to set your search engine
+// https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser/9851769
+var browser = function() {
+  // Return cached result if avalible, else get result then cache it.
+  if (browser.prototype._cachedResult)
+      return browser.prototype._cachedResult;
+  // Opera 8.0+
+  var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+  // Firefox 1.0+
+  var isFirefox = typeof InstallTrigger !== 'undefined';
+  // Safari 3.0+ "[object HTMLElementConstructor]" 
+  var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+  // Internet Explorer 6-11
+  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+  // Edge 20+
+  var isEdge = !isIE && !!window.StyleMedia;
+  // Chrome 1+
+  var isChrome = !!window.chrome && !!window.chrome.webstore;
+  // Blink engine detection
+  var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+  return browser.prototype._cachedResult =
+      isOpera ? 'Opera' :
+      isFirefox ? 'Firefox' :
+      isSafari ? 'Safari' :
+      isChrome ? 'Chrome' :
+      isIE ? 'Internet Explorer' :
+      isEdge ? 'Edge' :
+      isBlink ? 'Blink' :
+      'Chrome'; // give them Chrome instructions...better than nothing????
+};
