d2c553f52c82e37c6d319edc929e377587cf8d15
Brent Adamson <6355304+brentadamson@users.noreply.github.com>
2018-11-14 18:03:56 -0700

    even better test coverage

--- frontend/proxy_test.go
+++ frontend/proxy_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/jarcoal/httpmock"
 	"github.com/jivesearch/jivesearch/log"
 	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/css"
 	"github.com/tdewolff/minify/html"
 )
 
@@ -72,7 +73,7 @@ func TestProxyHandler(t *testing.T) {
 		u      string
 		key    string
 		secret string
-		html   string
+		resp   string
 	}
 
 	for _, c := range []struct {
@@ -87,26 +88,42 @@ func TestProxyHandler(t *testing.T) {
 				u:      "https://example.com",
 				key:    "jfsdijf89sd",
 				secret: "my_secret",
-				html: `<html>
+				resp: `<html>
 								<head>
+									<link rel="stylesheet" type="text/css" href="mystyle.css">
+									<style>
+										.body {margin:0}
+										#mydiv {background: lightblue url("img_tree.gif") no-repeat fixed center}
+									</style>									
 									<script>alert("this is dangerous!")</script>
 								</head>
 								<body>
-									<form id="form">
-									</form>
+									<form id="form"></form>
 									<a href="https://www.example.com">A link</a>
 									<a href="/relative/link">A relative link</a>
 									<iframe src="https://example.com/iframe/stuff"></iframe>
+									<img src="nice.jpg" alt="nice image">
+									<img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAggg==" alt="Red dot" />
+									<div style="background-image: url("paper.gif");">Cool div you got there. Would be a shame if we proxied the url.</div>
 								</body>
 							</html>`,
 			},
 			`<html>
-				<head></head>
+				<head>
+				<link rel=stylesheet href="/proxy?css=https%3A%2F%2Fexample.com%2Fmystyle.css&amp;key=3jUnkmdp2GQ0a9mmkFWYaTq6pg9rxGdVlic5t4fvfKc%3D">
+				<style>
+					.body {margin:0}
+					#mydiv {background: lightblue url("/image/,sUKGG_QlTynjPRkhces2ykv26GkZbya3NOhrjgMZCWXY=/https://example.com/img_tree.gif") no-repeat fixed center}
+				</style>
+				</head>
 				<body>
 					<form id=form disabled action=javascript:void(0);></form>
 					<a href="/proxy?key=_Zbla8JTucVtfb7n-QIGsrKozkTGaGsuKlxppnXb6xM%3D&amp;u=https%3A%2F%2Fwww.example.com" target=_top>A link</a>
 					<a href="/proxy?key=j_gIsLDElFG1Qnp3TAYn1KD5dwvJ0gB_KqvUjXvM64g%3D&amp;u=https%3A%2F%2Fexample.com%2Frelative%2Flink" target=_top>A relative link</a>
 					<iframe src="/proxy?iframe=true&amp;key=QtzD41Rkf5VUsmVPv9kSn4VHfUqf2jMljGktkjYVOVc%3D&amp;u=https%3A%2F%2Fexample.com%2Fiframe%2Fstuff"></iframe>
+					<img src="/image/,sypKZuwtHssDFg_bLaExLhx4rYNnbr0KkzPeekQYRlGA=/https://example.com/nice.jpg" alt="nice image">
+					<img src=data:image/png;base64 alt="Red dot">
+					<div style=background-image:url( paper.gif");">Cool div you got there. Would be a shame if we proxied the url.</div>
 				</body>
 			</html>`,
 		},
@@ -119,7 +136,7 @@ func TestProxyHandler(t *testing.T) {
 				ProxyClient: &http.Client{},
 			}
 
-			responder := httpmock.NewStringResponder(200, c.html)
+			responder := httpmock.NewStringResponder(200, c.resp)
 			httpmock.RegisterResponder("GET", c.args.u, responder)
 
 			req, err := http.NewRequest("GET", "/proxy", nil)
@@ -182,5 +199,6 @@ func TestProxyHandler(t *testing.T) {
 func htmlMinify(s string) (string, error) {
 	m := minify.New()
 	m.AddFunc("text/html", html.Minify)
+	m.AddFunc("text/css", css.Minify)
 	return m.String("text/html", s)
 }
